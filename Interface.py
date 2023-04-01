1f1758a8045f1238b30fd84b84ef3971ee05e216
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2023-04-01 10:17:07 +0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -3,3 +3,9 @@ class Window:
     def __init__(self):
         self.buttons = []
         print("Creation of object")
+    def load_data(self):
+        print("load")
+    def close(self):
+        print("close")
+    def start(self):
+        print("start")
