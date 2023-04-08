9349a3ed648a1e757f24c5508f526816c30b48e4
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2023-04-08 12:12:12 +0300

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,16 @@
+
+class Database:
+    def __init__(self):
+        self.data = []
+        #тут пока чтение из файла, видимо.
+        print("Creation of object")
+
+    def get_all_data(self, file_name):
+        print("load data from database to array with answers (X,y). Пока тут будет чтение из файлов")
+
+    def add_sample(self):
+        print("check sample and add if ok else msg + save data in broker")
+
+    def delete_sample(self):
+        print("ask again: Are you sure? and delete.")
+
--- ML.py
+++ ML.py
@@ -1,10 +1,38 @@
+import numpy as np
+import Database
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
 
-class Clusterisation:
-    def __init__(self):
+class Classification:
+    def __init__(self, database):
         self.eps = 0.1
+        self.dataBase = database
+        self.n_comp = 13
+        self.n_neigh = 4
 
-    def mainComponent(self):
-        print("PCA")
+    def get_prediction(self, sample):
+        # check_sample + send to brocker
+        data, ans = self.dataBase.get_all_data()
+        data = np.array([features_2d.flatten() for features_2d in data])
+        y = np.array(ans)
+        scaler = StandardScaler()
+        scaler.fit(data)
+        Bcancer_scaled = scaler.transform(data)
+        pca = PCA(n_components=self.n_comp)
+        pca.fit(Bcancer_scaled)
+        pca_bcancer = pca.transform(Bcancer_scaled)
+        component = np.array([pca_bcancer[:, i] for i in range(self.n_comp)]).transpose()
+        clf = Pipeline(
+            [
+                ("scaler", StandardScaler()),
+                # ("nca", NeighborhoodComponentsAnalysis()),
+                ("knn", KNeighborsClassifier(n_neighbors=self.n_neigh, weights='distance')),
+            ]
+        )
+        clf.fit(data, y)
+        # clf.predict_proba(X_test) # тут типа вероятности, пока не решили надо оно или нет
+        return clf.predict(sample)
 
-    def Clusterisation(self):
-        print("knn or depends on picture of 2-3 PCA comp")
+
