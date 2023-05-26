fc769e22ca167fd271d6ff341de2ae82578e1a20
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2023-05-26 13:40:19 +0300

    Add files via upload

--- ML.py
+++ ML.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler, Normalizer
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
-
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
 class Model:
     def __init__(self, database):
@@ -22,10 +22,11 @@ class Model:
         y = np.array(ans)
         clf = Pipeline(
             [
-                #("scaler1", StandardScaler()),
-                ("normalizer1", Normalizer()),
+                ("scaler1", StandardScaler()),
+                #("flatten", Flatten())
+                ("normalizer", Normalizer()),
                 ('pca', PCA(self.n_comp)),
-                ("scaler2", StandardScaler()),
+                ("scaler", StandardScaler()),
                 # ("nca", NeighborhoodComponentsAnalysis()),
                 ("knn", KNeighborsClassifier(n_neighbors=self.n_neigh, weights='distance')),
             ]
@@ -36,4 +37,38 @@ class Model:
 
     def get_prediction(self, sample):
         # clf.predict_proba(X_test) # тут типа вероятности, пока не решили надо оно или нет
+        sample = sample.flatten()
         return self.model.predict(sample)
+
+    def get_proba_prediction(self, sample):
+        sample = sample.flatten()
+        return self.model.predict_proba([sample])
+
+    def grid_search(self):
+        clf = Pipeline(
+            [
+                ("scaler1", StandardScaler()),
+                ("normalizer", Normalizer()),
+                ('pca', PCA()),
+                ("scaler", StandardScaler()),
+                # ("nca", NeighborhoodComponentsAnalysis()),
+                ("knn", KNeighborsClassifier(n_neighbors=self.n_neigh, weights='distance')),
+            ]
+        )
+        parameters = {
+            'pca__n_components': [3, 5, 7, 11, 13],
+            'clf___neighbors': [2, 3, 4, 5, 6],
+        }
+        kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+        gs = GridSearchCV(clf, parameters, cv=kf, n_jobs=-1, verbose=1)
+        all_data = self.dataBase.data
+        ans = self.dataBase.ans
+        all_data = np.array([features_2d.flatten() for features_2d in all_data])
+        gs.fit(all_data, ans)
+
+        print("Best score: %0.3f" % gs.best_score_)
+        print("Best parameters set:")
+        best_parameters = gs.best_estimator_.get_params()
+        for param_name in sorted(parameters.keys()):
+            print("\t%s: %r" % (param_name, best_parameters[param_name]))
--- main.py
+++ main.py
@@ -1,12 +1,66 @@
+import matplotlib.pyplot as plt
+
 import Metrics
 import ML
 import Database
+import numpy as np
+
+def get_nosed_data(all_data,p):
+    return all_data * np.random.uniform(1-p, 1+p, (len(all_data), len(all_data[0]), len(all_data[0][0])))
+
 
 if __name__ == '__main__':
-
     database = Database.Database()
     model1 = ML.Model(database)
     print("модель создана:", model1.get_model())
     Metrics.get_cross_validation_score(model1)
+    all_data = model1.dataBase.data
+    for p in [0.025, 0.25]:
+        nosed_data = get_nosed_data(all_data, p)
+        prediction = []
+        for el in nosed_data:
+            prediction.append(model1.get_proba_prediction(el))
+        prediction = np.array(prediction)
+        arr = np.amax(prediction, -1)
+        plt.plot(arr, "o", label="noise="+str(p))
+        plt.ylim([0, 1])
+    plt.title("max probability")
+    plt.legend()
+    plt.show()
 
+
+    nosed_data025 = get_nosed_data(all_data, 0.025)
+    nosed_data25 = get_nosed_data(all_data, 0.25)
+    prediction = []
+    #for el in all_data:
+    #    prediction.append(model1.get_proba_prediction(el))
+    #iddef = len(prediction)
+    for el in nosed_data025:
+        prediction.append(model1.get_proba_prediction(el))
+    idch = len(prediction)
+    for el in nosed_data25:
+        prediction.append(model1.get_proba_prediction(el))
+    prediction = np.array(prediction)
+    arr = np.amax(prediction, -1)
+    time_arr = [sum(arr[0:i])/(i) for i in range(1, len(arr))]
+    #plt.plot(range(1, iddef + 1), time_arr[0:iddef], "o", label="noise=" + str(0))
+    plt.plot(range(1, idch+1),time_arr[0:idch], "o", label="noise=" + str(0.025))
+    plt.plot(range(idch+1, len(time_arr)), time_arr[idch:-1], "o", label="noise=" + str(0.25))
+    #plt.ylim([0, 1])
+    plt.title("sum(max(probas))/len(probas)")
+    plt.legend()
+    plt.show()
+
+    for p in [0.025, 0.25]:
+        nosed_data = get_nosed_data(all_data, p)
+        prediction = []
+        for el in nosed_data:
+            prediction.append(model1.get_proba_prediction(el))
+        prediction = np.array(prediction)
+        arr = abs(2 * np.amax(prediction, -1) - 1 - np.amin(prediction, -1))
+        plt.plot(arr, "o", label="noise="+str(p))
+        plt.ylim([0, 1])
+    plt.title("minimal difference")
+    plt.legend()
+    plt.show()
 
