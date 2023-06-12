14472ceecf3bb15a1fbabfe26083189d9541534d
Алексей <55784938+aolychkin@users.noreply.github.com>
2023-06-12 21:56:42 +0300

    interface and test data

--- main.py
+++ main.py
@@ -1,66 +1,101 @@
-import matplotlib.pyplot as plt
+import os
+import dearpygui.dearpygui as dpg
 
-import Metrics
 import ML
 import Database
 import numpy as np
 
-def get_nosed_data(all_data,p):
-    return all_data * np.random.uniform(1-p, 1+p, (len(all_data), len(all_data[0]), len(all_data[0][0])))
+
+def get_samples(path_files, quality):
+    all_data = []
+    ans = []
+    for filename in os.listdir(path_files):
+        all_data.append(np.loadtxt(path_files + filename))
+        ans.append(quality)
+    return [all_data, ans]
+
+
+def analysis(path, retry=False):
+    data = np.loadtxt(path)
+    if retry:
+        dpg.add_text(f'Перезапуск {os.path.split(path)[-1]}!', parent="activity")
+    else:
+        dpg.add_text(f'{os.path.split(path)[-1]} успешно загружен!', parent="activity")
+    if np.shape(data) == (300, 8):
+        database = Database.Database()
+        model1 = ML.Model(database)
+        model1.get_model()
+
+        try:
+            dpg.add_text(f'Анализируем...', parent="activity")
+            prediction = model1.get_proba_prediction(data)
+            quality = np.argmax(prediction)
+
+            if quality == 0:
+                dpg.add_text(f'Качество кофе - ВЫСОКОЕ!', parent="activity", color=(54, 238, 24))
+            elif quality == 1:
+                dpg.add_text(f'Качество кофе - СРЕДНЕЕ!', parent="activity", color=(238, 217, 24))
+            else:
+                dpg.add_text(f'Качество кофе - НИЗКОЕ!', parent="activity", color=(238, 191, 24))
+        except:
+            dpg.add_text(f'[!] Ошибка анализа', parent="activity", color=(238, 24, 24))
+            dpg.add_button(label="повторить", callback=callback, user_data=path, parent="activity")
+            dpg.add_text(f'Попробуйте повторить операцию', parent="errors")
+    else:
+        dpg.add_text(f'[!] Ошибка чтения файла', parent="activity", color=(238, 24, 24))
+        dpg.add_button(label="повторить", callback=callback, user_data=path, parent="activity")
+        dpg.add_text(f'Поступили некорректные данные '
+                     f'\n Ожидалось shape(300, 8);'
+                     f'\n Получено shape{np.shape(data)}', parent="errors")
+    dpg.add_separator(parent='activity')
+
+
+def callback(sender, app_data, user_data):
+    if user_data is not None:
+        analysis(user_data, True)
+    else:
+        for item in app_data['selections']:
+            analysis(app_data['selections'][item])
+            dpg.add_text(f'Выбран {item}', parent="load")
+
+
+def cancel_callback(sender, app_data):
+    print('Cancel was clicked.')
+    print("Sender: ", sender)
+    print("App Data: ", app_data)
 
 
 if __name__ == '__main__':
-    database = Database.Database()
-    model1 = ML.Model(database)
-    print("модель создана:", model1.get_model())
-    Metrics.get_cross_validation_score(model1)
-    all_data = model1.dataBase.data
-    for p in [0.025, 0.25]:
-        nosed_data = get_nosed_data(all_data, p)
-        prediction = []
-        for el in nosed_data:
-            prediction.append(model1.get_proba_prediction(el))
-        prediction = np.array(prediction)
-        arr = np.amax(prediction, -1)
-        plt.plot(arr, "o", label="noise="+str(p))
-        plt.ylim([0, 1])
-    plt.title("max probability")
-    plt.legend()
-    plt.show()
+    dpg.create_context()
 
+    # подгружаем кириллицу и устанавливаем шрифт по умолчанию
+    with dpg.font_registry():
+        with dpg.font("src/Inter-VariableFont_slnt,wght.ttf", 17, default_font=True, id="Default font"):
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+    dpg.bind_font("Default font")
 
-    nosed_data025 = get_nosed_data(all_data, 0.025)
-    nosed_data25 = get_nosed_data(all_data, 0.25)
-    prediction = []
-    #for el in all_data:
-    #    prediction.append(model1.get_proba_prediction(el))
-    #iddef = len(prediction)
-    for el in nosed_data025:
-        prediction.append(model1.get_proba_prediction(el))
-    idch = len(prediction)
-    for el in nosed_data25:
-        prediction.append(model1.get_proba_prediction(el))
-    prediction = np.array(prediction)
-    arr = np.amax(prediction, -1)
-    time_arr = [sum(arr[0:i])/(i) for i in range(1, len(arr))]
-    #plt.plot(range(1, iddef + 1), time_arr[0:iddef], "o", label="noise=" + str(0))
-    plt.plot(range(1, idch+1),time_arr[0:idch], "o", label="noise=" + str(0.025))
-    plt.plot(range(idch+1, len(time_arr)), time_arr[idch:-1], "o", label="noise=" + str(0.25))
-    #plt.ylim([0, 1])
-    plt.title("sum(max(probas))/len(probas)")
-    plt.legend()
-    plt.show()
+    # создаем окно выбора файла
+    with dpg.file_dialog(directory_selector=False, show=False, callback=callback,
+                         id="file_dialog_id", width=620, height=350):
+        dpg.add_file_extension("", color=(217, 217, 217, 255))
+        dpg.add_file_extension(".txt", color=(150, 255, 150, 255))
 
-    for p in [0.025, 0.25]:
-        nosed_data = get_nosed_data(all_data, p)
-        prediction = []
-        for el in nosed_data:
-            prediction.append(model1.get_proba_prediction(el))
-        prediction = np.array(prediction)
-        arr = abs(2 * np.amax(prediction, -1) - 1 - np.amin(prediction, -1))
-        plt.plot(arr, "o", label="noise="+str(p))
-        plt.ylim([0, 1])
-    plt.title("minimal difference")
-    plt.legend()
-    plt.show()
+    # окно загрузки данных с носа
+    with dpg.window(label="Загрузка данных с носа", tag='load', width=260, height=220):
+        dpg.add_button(label="Выбрать файл", callback=lambda: dpg.show_item("file_dialog_id"),
+                       width=220, height=44)
 
+    # окно активности
+    with dpg.window(label="Активность", tag='activity', width=440, height=440, pos=[260, 0]):
+        pass
+
+    # окно отчета по ошибкам
+    with dpg.window(label="Отчет по ошибкам", tag='errors', width=260, height=220, pos=[0, 220]):
+        pass
+
+    dpg.create_viewport(title='Coffee analysis', width=700, height=440)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    # dpg.set_primary_window("Primary Window", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
